f09f801376e445190c39f5725b6676a9f55e8713
Cassandra Naomi <cat.monden@gmail.com>
2022-03-12 16:42:28 -0500

    ranged settings, worldtime shouldn't throw errors in test scenes

--- Assets/Scripts/World/WorldTime.cs
+++ Assets/Scripts/World/WorldTime.cs
@@ -34,6 +34,8 @@ public class WorldTime : MonoBehaviour
     public float nsunIntensity;
     public float dsunShadow;
     public float nsunShadow;
+
+    bool sunsInScene;
     // Start is called before the first frame update
     void Start()
     {
@@ -41,7 +43,6 @@ public class WorldTime : MonoBehaviour
         skybox = Instantiate(realSkybox);
         RenderSettings.skybox = skybox;
         GameObject dsunObj = GameObject.Find("_DaySunLight");
-        
         if (dsunObj != null)
         {
             dsun = dsunObj.GetComponent<Light>();
@@ -55,6 +56,10 @@ public class WorldTime : MonoBehaviour
             nsunIntensity = nsun.intensity;
             nsunShadow = nsun.shadowStrength;
         }
+        if (dsunObj != null && nsunObj != null)
+        {
+            sunsInScene = true;
+        }
         StartCoroutine(UpdateTime());
     }
 
@@ -66,112 +71,118 @@ public class WorldTime : MonoBehaviour
             currentTime += timeScale / SECONDS_TO_HOURS * timeUpdateDelay;
             currentTime %= MAX_TIME;
             skybox.SetFloat("_ClockTime", currentTime);
-            float t;
-            if (InRange(currentTime, 0, 5f))
+            if (sunsInScene)
             {
-                sunColor = nightColor;
-                nsunAngle = Mathf.Lerp(sunAngleClampInner, 180f - sunAngleClampInner, T(currentTime, -6f, 5f));
-                nsun.intensity = nsunIntensity;
-                dsun.intensity = 0f;
-                nsun.shadowStrength = nsunShadow;
-                dsun.shadowStrength = 0f;
+                UpdateSuns();
             }
-            else if (InRange(currentTime, 5f, 5.25f))
-            {
-                sunColor = Color.Lerp(nightColor, duskColor, T(currentTime, 5f, 5.25f));
-                nsunAngle = Mathf.Lerp(180f - sunAngleClampInner, 180f - sunAngleClampOuter, T(currentTime, 5f, 6f));
-                nsun.intensity = nsunIntensity;
-                dsun.intensity = 0f;
-                nsun.shadowStrength = Mathf.Lerp(nsunShadow, 0f, T(currentTime, 5f, 5.5f));
-                dsun.shadowStrength = 0f;
-            }
-            else if (InRange(currentTime, 5.25f, 5.75f))
-            {
-                sunColor = duskColor;
-                nsunAngle = Mathf.Lerp(180f - sunAngleClampInner, 180f - sunAngleClampOuter, T(currentTime, 5f, 6f));
-                dsunAngle = Mathf.Lerp(sunAngleClampOuter, sunAngleClampInner, T(currentTime, 5.25f, 6f));
-                dsun.intensity = Mathf.Lerp(0f, dsunIntensity, T(currentTime, 5.25f, 6f));
-                nsun.intensity = Mathf.Lerp(nsunIntensity, 0f, T(currentTime, 5.25f, 6f));
-                nsun.shadowStrength = Mathf.Lerp(nsunShadow, 0f, T(currentTime, 5f, 5.5f));
-                dsun.shadowStrength = Mathf.Lerp(0f, dsunShadow, T(currentTime, 5.5f, 6f));
-            }
-            else if (InRange(currentTime, 5.75f, 6f))
-            {
-                sunColor = Color.Lerp(duskColor, dayColor, T(currentTime, 5.75f, 6f));
-                nsunAngle = Mathf.Lerp(180f - sunAngleClampInner, 180f - sunAngleClampOuter, T(currentTime, 5f, 6f));
-                dsunAngle = Mathf.Lerp(sunAngleClampOuter, sunAngleClampInner, T(currentTime, 5.25f, 6f));
-                dsun.intensity = Mathf.Lerp(0f, dsunIntensity, T(currentTime, 5.25f, 6f));
-                nsun.intensity = Mathf.Lerp(nsunIntensity, 0f, T(currentTime, 5.25f, 6f));
-                nsun.shadowStrength = 0f;
-                dsun.shadowStrength = Mathf.Lerp(0f, dsunShadow, T(currentTime, 5.5f, 6f));
-            }
-            else if (InRange(currentTime, 6f, 17f))
-            {
-                sunColor = dayColor;
-                dsunAngle = Mathf.Lerp(sunAngleClampInner, 180f - sunAngleClampInner, T(currentTime, 6f, 17f));
-                dsun.intensity = dsunIntensity;
-                nsun.intensity = 0f;
-                nsun.shadowStrength = 0f;
-                dsun.shadowStrength = dsunShadow;
-            }
-            else if (InRange(currentTime, 17f, 17.25f))
-            {
-                sunColor = Color.Lerp(dayColor, duskColor, T(currentTime, 17f, 17.25f));
-                dsunAngle = Mathf.Lerp(180f - sunAngleClampInner, 180f - sunAngleClampOuter, T(currentTime, 17f, 18f));
-                dsun.intensity = dsunIntensity;
-                nsun.intensity = 0f;
-                dsun.shadowStrength = Mathf.Lerp(dsunShadow, 0f, T(currentTime, 17f, 17.75f));
-                nsun.shadowStrength = 0f;
-            }
-            else if (InRange(currentTime, 17.25f, 17.75f))
-            {
-                sunColor = duskColor;
-                dsunAngle = Mathf.Lerp(180f - sunAngleClampInner, 180f - sunAngleClampOuter, T(currentTime, 17f, 18f));
-                nsunAngle = Mathf.Lerp(sunAngleClampOuter, sunAngleClampInner, T(currentTime, 17.25f, 18f));
-                nsun.intensity = Mathf.Lerp(0f, nsunIntensity, T(currentTime, 17.25f, 18f));
-                dsun.intensity = Mathf.Lerp(dsunIntensity, 0f, T(currentTime, 17.25f, 18f));
-                dsun.shadowStrength = Mathf.Lerp(dsunShadow, 0f, T(currentTime, 17f, 17.75f));
-                nsun.shadowStrength = 0f;
-            }
-            else if (InRange(currentTime, 17.75f, 18f))
-            {
-                sunColor = Color.Lerp(duskColor, nightColor, T(currentTime, 17.75f, 18f));
-                dsunAngle = Mathf.Lerp(180f - sunAngleClampInner, 180f - sunAngleClampOuter, T(currentTime, 17f, 18f));
-                nsunAngle = Mathf.Lerp(sunAngleClampOuter, sunAngleClampInner, T(currentTime, 17.75f, 18f));
-                nsun.intensity = Mathf.Lerp(0f, nsunIntensity, T(currentTime, 17.25f, 18f));
-                dsun.intensity = Mathf.Lerp(dsunIntensity, 0f, T(currentTime, 17.25f, 18f));
-                dsun.shadowStrength = 0f;
-                nsun.shadowStrength = Mathf.Lerp(0f, nsunShadow, T(currentTime, 17.75f, 18f));
-            }
-            else if (InRange(currentTime, 18f, 24f))
-            {
-                sunColor = nightColor;
-                nsunAngle = Mathf.Lerp(sunAngleClampInner, 180f - sunAngleClampInner, T(currentTime, 18f, 29f));
-                nsun.intensity = nsunIntensity;
-                dsun.intensity = 0f;
-                dsun.shadowStrength = 0f;
-                nsun.shadowStrength = nsunShadow;
-            }
-            else
-            {
-                Debug.LogWarning("time is not within any time range!");
-            }
-            nsun.color = sunColor;
-            dsun.color = sunColor;
-            //sunAngle = ((currentTime + 6f) % 12f) / 12f;
-            //sunAngle = currentTime / 24f;
-            Vector3 sunDir = Vector3.right;
-            //sunDir = Quaternion.Euler(sunAngle * Mathf.PI * Mathf.Rad2Deg, 0f, 0f) * sunDir; Vector3.rot
-            dsun.transform.rotation = Quaternion.Euler(dsunAngle, 0f, 0f);
-            nsun.transform.rotation = Quaternion.Euler(nsunAngle, 0f, 0f);
-            dsun.enabled = (dsun.intensity != 0);
-            nsun.enabled = (nsun.intensity != 0);
-
             yield return new WaitForSecondsRealtime(timeUpdateDelay);
         }
         while (true);
     }
 
+    void UpdateSuns()
+    {
+        float t;
+        if (InRange(currentTime, 0, 5f))
+        {
+            sunColor = nightColor;
+            nsunAngle = Mathf.Lerp(sunAngleClampInner, 180f - sunAngleClampInner, T(currentTime, -6f, 5f));
+            nsun.intensity = nsunIntensity;
+            dsun.intensity = 0f;
+            nsun.shadowStrength = nsunShadow;
+            dsun.shadowStrength = 0f;
+        }
+        else if (InRange(currentTime, 5f, 5.25f))
+        {
+            sunColor = Color.Lerp(nightColor, duskColor, T(currentTime, 5f, 5.25f));
+            nsunAngle = Mathf.Lerp(180f - sunAngleClampInner, 180f - sunAngleClampOuter, T(currentTime, 5f, 6f));
+            nsun.intensity = nsunIntensity;
+            dsun.intensity = 0f;
+            nsun.shadowStrength = Mathf.Lerp(nsunShadow, 0f, T(currentTime, 5f, 5.5f));
+            dsun.shadowStrength = 0f;
+        }
+        else if (InRange(currentTime, 5.25f, 5.75f))
+        {
+            sunColor = duskColor;
+            nsunAngle = Mathf.Lerp(180f - sunAngleClampInner, 180f - sunAngleClampOuter, T(currentTime, 5f, 6f));
+            dsunAngle = Mathf.Lerp(sunAngleClampOuter, sunAngleClampInner, T(currentTime, 5.25f, 6f));
+            dsun.intensity = Mathf.Lerp(0f, dsunIntensity, T(currentTime, 5.25f, 6f));
+            nsun.intensity = Mathf.Lerp(nsunIntensity, 0f, T(currentTime, 5.25f, 6f));
+            nsun.shadowStrength = Mathf.Lerp(nsunShadow, 0f, T(currentTime, 5f, 5.5f));
+            dsun.shadowStrength = Mathf.Lerp(0f, dsunShadow, T(currentTime, 5.5f, 6f));
+        }
+        else if (InRange(currentTime, 5.75f, 6f))
+        {
+            sunColor = Color.Lerp(duskColor, dayColor, T(currentTime, 5.75f, 6f));
+            nsunAngle = Mathf.Lerp(180f - sunAngleClampInner, 180f - sunAngleClampOuter, T(currentTime, 5f, 6f));
+            dsunAngle = Mathf.Lerp(sunAngleClampOuter, sunAngleClampInner, T(currentTime, 5.25f, 6f));
+            dsun.intensity = Mathf.Lerp(0f, dsunIntensity, T(currentTime, 5.25f, 6f));
+            nsun.intensity = Mathf.Lerp(nsunIntensity, 0f, T(currentTime, 5.25f, 6f));
+            nsun.shadowStrength = 0f;
+            dsun.shadowStrength = Mathf.Lerp(0f, dsunShadow, T(currentTime, 5.5f, 6f));
+        }
+        else if (InRange(currentTime, 6f, 17f))
+        {
+            sunColor = dayColor;
+            dsunAngle = Mathf.Lerp(sunAngleClampInner, 180f - sunAngleClampInner, T(currentTime, 6f, 17f));
+            dsun.intensity = dsunIntensity;
+            nsun.intensity = 0f;
+            nsun.shadowStrength = 0f;
+            dsun.shadowStrength = dsunShadow;
+        }
+        else if (InRange(currentTime, 17f, 17.25f))
+        {
+            sunColor = Color.Lerp(dayColor, duskColor, T(currentTime, 17f, 17.25f));
+            dsunAngle = Mathf.Lerp(180f - sunAngleClampInner, 180f - sunAngleClampOuter, T(currentTime, 17f, 18f));
+            dsun.intensity = dsunIntensity;
+            nsun.intensity = 0f;
+            dsun.shadowStrength = Mathf.Lerp(dsunShadow, 0f, T(currentTime, 17f, 17.75f));
+            nsun.shadowStrength = 0f;
+        }
+        else if (InRange(currentTime, 17.25f, 17.75f))
+        {
+            sunColor = duskColor;
+            dsunAngle = Mathf.Lerp(180f - sunAngleClampInner, 180f - sunAngleClampOuter, T(currentTime, 17f, 18f));
+            nsunAngle = Mathf.Lerp(sunAngleClampOuter, sunAngleClampInner, T(currentTime, 17.25f, 18f));
+            nsun.intensity = Mathf.Lerp(0f, nsunIntensity, T(currentTime, 17.25f, 18f));
+            dsun.intensity = Mathf.Lerp(dsunIntensity, 0f, T(currentTime, 17.25f, 18f));
+            dsun.shadowStrength = Mathf.Lerp(dsunShadow, 0f, T(currentTime, 17f, 17.75f));
+            nsun.shadowStrength = 0f;
+        }
+        else if (InRange(currentTime, 17.75f, 18f))
+        {
+            sunColor = Color.Lerp(duskColor, nightColor, T(currentTime, 17.75f, 18f));
+            dsunAngle = Mathf.Lerp(180f - sunAngleClampInner, 180f - sunAngleClampOuter, T(currentTime, 17f, 18f));
+            nsunAngle = Mathf.Lerp(sunAngleClampOuter, sunAngleClampInner, T(currentTime, 17.75f, 18f));
+            nsun.intensity = Mathf.Lerp(0f, nsunIntensity, T(currentTime, 17.25f, 18f));
+            dsun.intensity = Mathf.Lerp(dsunIntensity, 0f, T(currentTime, 17.25f, 18f));
+            dsun.shadowStrength = 0f;
+            nsun.shadowStrength = Mathf.Lerp(0f, nsunShadow, T(currentTime, 17.75f, 18f));
+        }
+        else if (InRange(currentTime, 18f, 24f))
+        {
+            sunColor = nightColor;
+            nsunAngle = Mathf.Lerp(sunAngleClampInner, 180f - sunAngleClampInner, T(currentTime, 18f, 29f));
+            nsun.intensity = nsunIntensity;
+            dsun.intensity = 0f;
+            dsun.shadowStrength = 0f;
+            nsun.shadowStrength = nsunShadow;
+        }
+        else
+        {
+            Debug.LogWarning("time is not within any time range!");
+        }
+        nsun.color = sunColor;
+        dsun.color = sunColor;
+        //sunAngle = ((currentTime + 6f) % 12f) / 12f;
+        //sunAngle = currentTime / 24f;
+        Vector3 sunDir = Vector3.right;
+        //sunDir = Quaternion.Euler(sunAngle * Mathf.PI * Mathf.Rad2Deg, 0f, 0f) * sunDir; Vector3.rot
+        dsun.transform.rotation = Quaternion.Euler(dsunAngle, 0f, 0f);
+        nsun.transform.rotation = Quaternion.Euler(nsunAngle, 0f, 0f);
+        dsun.enabled = (dsun.intensity != 0);
+        nsun.enabled = (nsun.intensity != 0);
+    }
     private void OnDestroy()
     {
         Destroy(skybox);
