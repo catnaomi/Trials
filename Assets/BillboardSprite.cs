0648a6b713023f1a5cb60d9f15733205392bc777
Cassandra Naomi <cat.monden@gmail.com>
2022-06-07 15:50:16 -0400

    ice golem imports pt 1

--- Assets/BillboardSprite.cs
+++ Assets/BillboardSprite.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//[ExecuteInEditMode]
+[ExecuteInEditMode]
 public class BillboardSprite : MonoBehaviour
 {
     private void Update()
     {
 
-        this.transform.forward = -Camera.main.transform.forward;
+        if (Camera.main != null)
+        {
+            this.transform.forward = -Camera.main.transform.forward;
+        }
+        else if (Camera.current != null)
+        {
+            this.transform.forward = -Camera.current.transform.forward;
+        }
         
     }
 }
