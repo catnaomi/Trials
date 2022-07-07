46ed3f40a9174b6f6ab92284d02e8b2700b34607
Cassandra Naomi <cat.monden@gmail.com>
2022-07-07 16:16:00 -0400

    two panel maintain view

---
+++ Assets/Scripts/TwoPanelMaintainView.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoPanelMaintainView : MonoBehaviour
+{
+    public Transform origin;
+    public Transform front;
+    public Transform back;
+    public float width;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
