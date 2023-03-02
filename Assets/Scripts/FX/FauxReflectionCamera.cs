00082039b5747780bec67b14787afba7db2a1134
Cassandra Naomi <cat.monden@gmail.com>
2023-03-02 15:15:59 -0500

    water scene shader progress

--- Assets/Scripts/FX/FauxReflectionCamera.cs
+++ Assets/Scripts/FX/FauxReflectionCamera.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,17 +6,31 @@ using UnityEngine;
 [ExecuteAlways]
 public class FauxReflectionCamera : MonoBehaviour
 {
+    [Header("Camera Settings")]
     public float mirrorHeight = 0f;
     Plane plane;
+    [Header("Rendering Settings")]
+    public Renderer[] reflectedRenderers;
+    RenderTexture rt;
+    MaterialPropertyBlock block;
     // Start is called before the first frame update
     void Start()
     {
-        plane = new Plane(Vector3.up, new Vector3(0, mirrorHeight, 0));   
+        plane = new Plane(Vector3.up, new Vector3(0, mirrorHeight, 0));
+        rt = new RenderTexture(1024, 576, 24);
+        block = new MaterialPropertyBlock();
+        block.SetTexture("_ReflectionMap", rt);
+        foreach(Renderer r in reflectedRenderers)
+        {
+            r.SetPropertyBlock(block);
+        }
+        this.GetComponent<Camera>().targetTexture = rt;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         Vector3 mainHeading = Camera.main.transform.forward;
         Vector3 planeProject = Vector3.ProjectOnPlane(mainHeading, Vector3.up);
         Vector3 reflectedHeading = Quaternion.AngleAxis(180f, planeProject) * mainHeading;
@@ -24,5 +39,10 @@ public class FauxReflectionCamera : MonoBehaviour
 
         float dist = Camera.main.transform.position.y - mirrorHeight;
         this.transform.position = Camera.main.transform.position - (Vector3.up * dist * 2f);
+    }
+
+    private void OnDestroy()
+    {
+        //Destroy(rt);
     }
 }
---
+++ Assets/Scripts/FX/OpaqueWhenApproachedShaderHelper.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpaqueWhenApproachedShaderHelper : MonoBehaviour
+{
+    Renderer renderer;
+    MaterialPropertyBlock block;
+    // Start is called before the first frame update
+    void Start()
+    {
+        block = new MaterialPropertyBlock();
+        renderer = this.GetComponent<Renderer>();
+        renderer.SetPropertyBlock(block);
+        block.SetVector("_PlayerPosition", Vector3.zero);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PlayerActor.player != null)
+        {
+            block.SetVector("_PlayerPosition", PlayerActor.player.transform.position);
+            renderer.SetPropertyBlock(block);
+        }
+    }
+}
