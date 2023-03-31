a9aca0c90111f7d8d44e39b6153aa352ab9776ab
Cassandra Naomi <cat.monden@gmail.com>
2023-03-31 19:22:14 -0400

    auto ledge linking

---
+++ Assets/Editor/LedgeEditor.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Ledge))]
+public class LedgeEditor : Editor
+{
+    Vector3 destination = Vector3.zero;
+    GameObject obj = null;
+
+    public override void OnInspectorGUI()
+    {
+        Ledge ledge = (Ledge)target;
+        DrawDefaultInspector();
+        GUILayout.Space(10f);
+        if (GUILayout.Button("Validate Links"))
+        {
+            ledge.ValidateLinks();
+        }
+        GUILayout.Space(1f);
+        if (GUILayout.Button("Auto Connect Links"))
+        {
+            ledge.ValidateThenAutoConnectLedge();
+        }
+        GUILayout.Space(10f);
+        if (GUILayout.Button("Validate Links (All)"))
+        {
+            Ledge.ValidateAllLedges();
+        }
+        GUILayout.Space(1f);
+        if (GUILayout.Button("Auto Connect Links (All)"))
+        {
+            Ledge.AutoConnectLedges();
+        }
+    }
+}
--- Assets/Scripts/Climbing/Ledge.cs
+++ Assets/Scripts/Climbing/Ledge.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Ledge : ClimbDetector
 {
+    readonly float MAX_AUTO_LEDGE_DISTANCE = 1f;
     Rigidbody ledge;
 
     public Transform snap;
@@ -17,12 +18,14 @@ public class Ledge : ClimbDetector
     public Ledge left;
     public bool linkedRight;
     public Ledge right;
+    bool isLeftLinkValid;
+    bool isRightLinkValid;
     // Use this for initialization
     void Awake()
     {
         ledge = this.GetComponent<Rigidbody>();
         collider = this.GetComponent<Collider>();
-
+        ValidateLinks();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -60,9 +63,13 @@ public class Ledge : ClimbDetector
 
     public Vector3 GetSnapPoint(float climberWidth)
     {
-        return snap.transform.position + snap.transform.right * -snapPoint * (GetLength() - climberWidth) * 0.5f;
+        return GetSnapPointForValue(climberWidth, this.snapPoint);
     }
 
+    public Vector3 GetSnapPointForValue(float climberWidth, float snapPoint)
+    {
+        return snap.transform.position + snap.transform.right * -snapPoint * (GetLength() - climberWidth) * 0.5f;
+    }
 
     public Vector3 GetSnapPointDot(float climberWidth, Vector3 climberPosition, PlayerActor player, int dir)
     {
@@ -92,21 +99,176 @@ public class Ledge : ClimbDetector
         return GetSnapPoint(climberWidth);
     }
 
+    private readonly float debug_climberwidth = 0.5f;
+    public Vector3 GetLeftEnd()
+    {
+        return GetSnapPointForValue(debug_climberwidth, 1f);
+    }
+
+    public Vector3 GetRightEnd()
+    {
+        return GetSnapPointForValue(debug_climberwidth, -1f);
+    }
     private void OnValidate()
     {
-        if (linkedLeft && left != null)
+        ValidateLinks();
+    }
+
+    public void ValidateLinks()
+    {
+        isLeftLinkValid = true;
+        if (linkedLeft)
         {
-            if (!left.linkedRight || left.right != this)
+            if (left == null)
+            {
+                Debug.LogWarning(string.Format("{0} is linked on left side with no connecting ledge", this));
+                isLeftLinkValid = false;
+            }
+            else if (!left.linkedRight || left.right != this)
             {
                 Debug.LogWarning(string.Format("{0} not properly linked to {1}", this, left));
+                isLeftLinkValid = false;
+            }
+            else if (left == this)
+            {
+                Debug.LogWarning(string.Format("{0} is linked to self", this));
+                isLeftLinkValid = false;
             }
         }
-        if (linkedRight && right != null)
+        isRightLinkValid = true;
+        if (linkedRight)
         {
-            if (!right.linkedLeft || right.left != this)
+            if (right == null)
+            {
+                Debug.LogWarning(string.Format("{0} is linked on right side with no connecting ledge", this));
+                isRightLinkValid = false;
+            }
+            else if (!right.linkedLeft || right.left != this)
             {
                 Debug.LogWarning(string.Format("{0} not properly linked to {1}", this, right));
+                isRightLinkValid = false;
+            }
+            else if (right == this)
+            {
+                Debug.LogWarning(string.Format("{0} is linked to self", this));
+                isRightLinkValid = false;
             }
         }
+    }
+    private void OnDrawGizmos()
+    {
+        if (linkedLeft)
+        {
+            Gizmos.color = isLeftLinkValid ? Color.yellow : Color.magenta;
+            if (left != null)
+            {
+                Gizmos.DrawLine(this.GetLeftEnd()+Vector3.up*0.01f, left.GetRightEnd());
+            }
+            Gizmos.DrawWireSphere(GetLeftEnd(), 0.1f);
+        }
+        if (linkedRight)
+        {
+            Gizmos.color = isRightLinkValid ? Color.cyan : Color.magenta;
+            if (right != null)
+            {
+                Gizmos.DrawLine(this.GetRightEnd() + Vector3.up * -0.01f, right.GetLeftEnd());
+            }
+            Gizmos.DrawWireSphere(GetRightEnd(), 0.1f);
+        }
+    }
+
+    public static void AutoConnectLedges()
+    {
+        
+        Ledge[] ledges = FindObjectsOfType<Ledge>();
+
+
+        Debug.Log("validating ledge links");
+        // check validity of all ledge links before editing
+        foreach (Ledge validatingLedge in ledges)
+        {
+         
+            validatingLedge.ValidateLinks();
+        }
+
+        Debug.Log("auto linking ledges");
+
+        foreach (Ledge workingLedge in ledges)
+        {
+            workingLedge.AutoConnectLedge();
+        }
+    }
+
+    public static void ValidateAllLedges()
+    {
+        Ledge[] ledges = FindObjectsOfType<Ledge>();
+
+
+        Debug.Log("validating ledge links");
+        // check validity of all ledge links before editing
+        foreach (Ledge validatingLedge in ledges)
+        {
+            validatingLedge.ValidateLinks();
+        }
+    }
+
+    public void AutoConnectLedge()
+    {
+        
+        bool shouldCheckLeft = !this.isLeftLinkValid || !this.linkedLeft;
+        bool shouldCheckRight = !this.isRightLinkValid || !this.linkedRight;
+
+        if (shouldCheckLeft || shouldCheckRight)
+        {
+            Ledge[] ledges = FindObjectsOfType<Ledge>();
+            bool foundLeftLink = false;
+            bool foundRightLink = false;
+            // left side
+            foreach (Ledge connectingLedge in ledges)
+            {
+                if (connectingLedge == this) continue;
+                if (shouldCheckLeft && !foundLeftLink)
+                {
+                    if (Vector3.Distance(this.GetLeftEnd(), connectingLedge.GetRightEnd()) <= MAX_AUTO_LEDGE_DISTANCE)
+                    {
+                        this.left = connectingLedge;
+                        connectingLedge.right = this;
+                        this.isLeftLinkValid = true;
+                        connectingLedge.isRightLinkValid = true;
+                        foundLeftLink = true;
+                        this.linkedLeft = true;
+                        connectingLedge.linkedRight = true;
+                    }
+                }
+                else if (shouldCheckRight && !foundRightLink)
+                {
+                    if (Vector3.Distance(this.GetRightEnd(), connectingLedge.GetLeftEnd()) <= MAX_AUTO_LEDGE_DISTANCE)
+                    {
+                        this.right = connectingLedge;
+                        connectingLedge.left = this;
+                        this.isRightLinkValid = true;
+                        connectingLedge.isLeftLinkValid = true;
+                        foundRightLink = true;
+                        this.linkedRight = true;
+                        connectingLedge.linkedLeft = true;
+                    }
+                }
+            }
+            if (shouldCheckLeft && !foundLeftLink)
+            {
+                this.linkedLeft = false;
+            }
+            if (shouldCheckRight && !foundRightLink)
+            {
+                this.linkedRight = false;
+            }
+        }
+        
+    }
+
+    public void ValidateThenAutoConnectLedge()
+    {
+        ValidateLinks();
+        AutoConnectLedge();
     }
 }
