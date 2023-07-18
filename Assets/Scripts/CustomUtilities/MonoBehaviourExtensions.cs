ea7b8bba6a5106ffcc4b04c5540f80d6889b2da7
Cassandra Naomi <cat.monden@gmail.com>
2023-07-18 14:31:13 -0400

    finally made a damn timer method

---
+++ Assets/Scripts/CustomUtilities/MonoBehaviourExtensions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using System.Collections;
+
+namespace CustomUtilities
+{
+    public static class MonoBehaviourExtensions
+    {
+
+
+        public static Coroutine StartTimer(this MonoBehaviour mono, float duration, bool loops, UnityAction callback)
+        {
+            Coroutine routine = mono.StartCoroutine(TimerRoutine(duration, loops, callback));
+            return routine;
+        }
+
+        static IEnumerator TimerRoutine(float duration, bool loops, UnityAction callback)
+        {
+            do {
+                yield return new WaitForSecondsRealtime(duration);
+                callback.Invoke();
+            } while (loops);
+        }
+    }
+}
