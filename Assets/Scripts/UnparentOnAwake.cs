6dc33fd8e4fc3fb7d5692c6eb8a975d54a3e8252
Cassandra Naomi <cat.monden@gmail.com>
2023-08-28 17:27:25 -0400

    add ice giant to dojo scene

--- Assets/Scripts/UnparentOnAwake.cs
+++ Assets/Scripts/UnparentOnAwake.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class UnparentOnAwake : MonoBehaviour
 {
+    public bool alsoDisable;
     private void Awake()
     {
         if (this.transform.parent != null)
         {
             this.transform.SetParent(null, true);
         }
+        if (alsoDisable)
+        {
+            this.StartCoroutine(DisableAfterFirstFrame());
+        }
+    }
+
+    IEnumerator DisableAfterFirstFrame()
+    {
+        yield return new WaitForEndOfFrame();
+        this.gameObject.SetActive(false);
     }
 }
